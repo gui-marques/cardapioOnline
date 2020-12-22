e46c08502b89ba999fc68f200782f7ba2c34943c
Guilherme <gui.slayon@live.com>
2020-12-22 16:58:49 -0300

    first commit

---
+++ src/components/SliderData.js
@@ -0,0 +1,51 @@
+export const SliderData = [
+
+    {
+        image: 'https://i.imgur.com/FW6zWj3.png',
+
+    },
+    
+    {
+        image: 'https://i.imgur.com/5BQXQ4z.png',
+
+    },
+
+    
+    {
+        image: 'https://i.imgur.com/cioFK5g.png',
+
+    },
+
+    {
+        image: 'https://i.imgur.com/qjCOiNh.png',
+
+    },
+    {
+        image: 'https://i.imgur.com/30mq1eI.png',
+
+    },
+    {
+        image: 'https://i.imgur.com/k2kmFgz.png',
+
+    }, 
+     {
+        image: 'https://i.imgur.com/EZpB6TK.png',
+
+    },
+    {
+        image: 'https://i.imgur.com/KWvJEIU.png',
+
+    },
+    {
+        image: 'https://i.imgur.com/aqwSXOX.png',
+
+    },    
+    {
+        image: 'https://i.imgur.com/0GsdoLr.png',
+
+    },
+    {
+        image: 'https://i.imgur.com/C9Pa4zb.png',
+
+    },
+]
